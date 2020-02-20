535ca1dec9d4a3fa765b71e85e4bdbf5f222eabc
Leah H <harrichl@gmail.com>
2020-02-20 17:21:11 -0500

    implement firebase email/password signup

--- src/pages/sign-in-sign-up.js
+++ src/pages/sign-in-sign-up.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import SignIn from '../components/sign-in/sign-in.component';
+import SignUp from '../components/sign-up/sign-up.component';
+
 
 import './sign-in-sign-up.css';
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-sign-up'><SignIn /></div>
+    <div className='sign-in-sign-up'>
+        <SignIn />
+        <SignUp />
+    </div>
 );
 
 export default SignInAndSignUpPage;
